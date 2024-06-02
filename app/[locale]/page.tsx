aa08f3e23a88964ee2167d259e8be29bd3d6c3b7
twy8939 <twy8939@naver.com>
2024-06-02 22:57:13 +0900

    feat : 동적 localize

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 import { unstable_setRequestLocale } from "next-intl/server";
 import WeatherAlerts from "../_components/WeatherAlerts/WeatherAlerts";
+import { useTranslations } from "next-intl";
 
 export default function Home({
   params: { locale },
@@ -7,8 +8,15 @@ export default function Home({
   params: { locale: string };
 }) {
   unstable_setRequestLocale(locale);
+
+  const t = useTranslations("Home");
+
   return (
     <main className="min-h-screen">
+      <p className="mx-auto mb-2 w-max rounded-sm bg-slate-800 px-2 py-1 text-xs text-sky-200">
+        {t("userGreeting", { name: "YongMin" })}
+      </p>
+
       <h1 className="text-xs font-thin">Today&apos;s weather</h1>
       <h2 className="text-lg">Tuesday May 21 2024</h2>
       <section>
