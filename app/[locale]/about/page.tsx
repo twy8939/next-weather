bd3cafa0ecc042da0c293ab84ab27c4386b448e3
twy8939 <twy8939@naver.com>
2024-06-08 00:50:39 +0900

    feat : include HTML

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -10,7 +10,16 @@ export default function About({
   const t = useTranslations("About");
   return (
     <main className="min-h-screen">
-      <h1 className="text-xs font-thin">About</h1>
+      <h1 className="text-xs font-thin">{t("title")}</h1>
+      <p className="mb-3 text-lg">
+        {t.rich("description", {
+          linkToNext: (chunks) => (
+            <a href="https://nextjs.org" className="text-sky-200 underline">
+              {chunks}
+            </a>
+          ),
+        })}
+      </p>
       <p className="text-lg">{t("liveDuration", { duration: 17200000.2 })}</p>
     </main>
   );
